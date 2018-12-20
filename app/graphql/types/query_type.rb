7cea45dbe9b93596618328b0732ee97922ffc8eb
Yoko Harada <yokolet@gmail.com>
2018-12-20 00:36:22 -0500

    feat: updates query_type.rb
    The template for query is already there. However, it is just a template.
    This comment adds a real query to user type.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,20 @@
-module Types
-  class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+Types::QueryType = GraphQL::ObjectType.define do
+  name "Query"
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
+  field :allUsers do
+    type types[Types::UserType]
+    description "returns a list of all users"
+    resolve -> (obj, args, ctx) {
+      User.all
+    }
+  end
+
+  field :user do
+    type Types::UserType
+    description "returns a user"
+    argument :id, !types.ID
+    resolve -> (obj, args, ctx) {
+      User.find(args[:id])
+    }
   end
 end
