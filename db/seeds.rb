db55e74119f85e040c10d96596f6c4cb4982bc5d
Yoko Harada <yokolet@gmail.com>
2018-12-19 22:15:08 -0500

    feat: adds user model seed data
    To test a newly created user model, seed data has been added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'Luke', email: 'luke@example.com')
+User.create(name: 'Gandalf', email: 'gandalf@example.com')
