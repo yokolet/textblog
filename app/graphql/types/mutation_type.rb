46ee3f8368ac6434468f63f37697c3b7026951ac
Yoko Harada <yokolet@gmail.com>
2018-12-21 17:26:02 -0500

    feat: adds three mutations
    Three mutations for User, create/update/destroy, were added.
    Those worked on GraphiQL.

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,30 @@
-module Types
-  class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+Types::MutationType = GraphQL::ObjectType.define do
+  name "Mutation"
+
+  field :createUser, Types::UserType do
+    argument :name, !types.String
+    argument :email, !types.String
+    resolve -> (obj, args, ctx) {
+      User.create(name: args[:name], email: args[:email])
+    }
+  end
+
+  field :updateUser, Types::UserType do
+    argument :id, !types.ID
+    argument :name, types.String
+    argument :email, types.String
+    resolve -> (obj, args, ctx) {
+      user = User.find(args[:id])
+      user.update!(args.to_h)
+      user
+    }
+  end
+
+  field :destroyUser, Types::UserType do
+    argument :id, !types.ID
+    resolve -> (obj, args, ctx) {
+      User.find(args[:id]).destroy!
+      nil
+    }
   end
 end
