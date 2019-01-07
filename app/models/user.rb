7c9d21c83879f035b36fa542be6ba5621602d27c
Yoko Harada <yokolet@gmail.com>
2019-01-06 23:51:33 -0500

    feat: makes user model omniauthable
    This is a small commit but an important change. The user model is now
    omniauthable. New paths for OAuth were created automatically by this
    change.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
   # validation
   validates_presence_of :name, :email
 end
