3c6ffd145c4e17d1e52f350526f0bdf87f35e671
Yoko Harada <yokolet@gmail.com>
2019-01-07 00:13:16 -0500

    feat: adds devise controller path
    The devise controllers were added to the routes.rb file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
