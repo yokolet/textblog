53eb355fa4611ebc2cb6a6ca399a691c04e27e44
Yoko Harada <yokolet@gmail.com>
2019-01-07 22:12:16 -0500

    feat: adds callback method to User
    The callback method to create a new user from returned parameters by
    OAuth was added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:facebook]
   # validation
   validates_presence_of :name, :email
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.name = auth.info.name
+      user.token = auth.credentials.token
+      user.token_expires_at = auth.credentials.expires_at
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
