ab645ec65a29001347e00a8a1f4337cfd731cee2
Yoko Harada <yokolet@gmail.com>
2019-01-07 22:10:58 -0500

    feat: creates a new controller for callback
    A omniauth callback controller was created. The callback methods were
    implemented as well.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,15 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    callback_for(:facebook)
+  end
 
+  def failure
+    redirect_to root_path
+  end
+
+  private
+  def callback_for(provider)
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    sign_in_and_redirect user, event: :authentication if user.persisted?
   end
 end
