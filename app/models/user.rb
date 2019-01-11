26edb8205a0705e05e68d3227e7267019d298c69
Yoko Harada <yokolet@gmail.com>
2019-01-10 23:49:13 -0500

    fix: removes unwanted modules and assignments
    The plan has changed to focus on only omniauth. The authentication will
    be token base. For this reason, many of devise modules were deleted. In
    light of schema change, some of field assignments were deleted as well.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook]
+  # Include devise modules.
+  devise :omniauthable, omniauth_providers: [:facebook]
   # validation
   validates_presence_of :name, :email
 
@@ -11,9 +8,6 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.name = auth.info.name
-      user.token = auth.credentials.token
-      user.token_expires_at = auth.credentials.expires_at
-      user.password = Devise.friendly_token[0,20]
     end
   end
 end
