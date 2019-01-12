c0e1bdb7c3ebf4ec208ebcaecb24759225700224
Yoko Harada <yokolet@gmail.com>
2019-01-11 23:24:05 -0500

    feat: adds signInUser field in mutation type
    A new mutation field, signInUser, was added. This mutation gets access
    token and Koala instaince from context (added in context by controller).
    Making a Facebook Graph API call, it verifies a user and returns user
    info.

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,27 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
+  field :signInUser, Types::UserType do
+    resolve -> (obj, args, ctx) {
+      access_token, koala = ctx[:api][:access_token], ctx[:api][:koala]
+      if access_token.nil?
+        raise GraphQL::ExecutionError.new('Authorization request header is missing.')
+      end
+      if koala.nil?
+        raise GraphQL::ExecutionError.new('Authorization request header is invalid.')
+      end
+      begin
+        me = koala.get_object('me', {'fields': 'id,name,email'}) # this line may raise error
+        User.where(provider: 'facebook', uid: me['id']).first_or_create do |user|
+          user.name = me['name']
+          user.email = me['email']
+        end
+      rescue => e
+        GraphQL::ExecutionError.new(e.message)
+      end
+    }
+  end
+
   field :createUser, Types::UserType do
     argument :name, !types.String
     argument :email, !types.String
