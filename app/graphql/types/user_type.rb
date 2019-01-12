4f9f258a2e20cf1ed94198ae6aa69365d5eea9d4
Yoko Harada <yokolet@gmail.com>
2019-01-11 23:26:59 -0500

    feat: adds provider and uid fields
    When the sever side OAuth flow was implemented, provider and uid columns
    were added to the model. To make it consistent, those were added to
    user_type schema.

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,6 +2,8 @@ Types::UserType = GraphQL::ObjectType.define do
   name "User"
 
   field :id, !types.ID
+  field :provider, !types.String
+  field :uid, !types.ID
   field :name, !types.String
   field :email, !types.String
 end
