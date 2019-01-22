272173713588813626046c5f9c3bef0f073a16b1
Yoko Harada <yokolet@gmail.com>
2019-01-21 22:44:35 -0500

    feat: adds user mutation type spec
    At this moment, only one mutation 'signInUser' is defined. For this
    type, field, resovler, and mutation string were tested.

---
+++ spec/graphql/mutations/user_mutation_type_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe Mutations::UserMutationType do
+  types = GraphQL::Define::TypeDefiner.instance
+
+  it 'defines a field signInUser that returns Types::UserType type' do
+    expect(subject).to have_a_field(:signInUser).that_returns(Types::UserType)
+  end
+
+  context 'with signInUser field' do
+    let(:facebook) {Faker::Omniauth.facebook }
+    let(:access_token) { facebook[:credentials][:token] }
+    let(:me) {
+      {
+          "id" => facebook[:uid],
+          "name" => facebook[:info][:name],
+          "email" => facebook[:info][:email]
+      }
+    }
+    let(:social_api) { double("social_api") }
+    let(:args) { {provider: 'facebook'} }
+    let(:ctx) { {
+        api: {
+            access_token: access_token,
+            social_api: social_api
+        }
+    } }
+
+    it 'create or find a user' do
+      allow(social_api).to receive(:get_object).and_return(me)
+      result = subject.fields['signInUser'].resolve(nil, args, ctx)
+      expect(result.id).not_to be_nil
+    end
+
+    describe 'a mutation is given' do
+      let(:vars) { {provider: "facebook"} }
+      let(:result) { TextblogSchema.execute(mutate_string, variables: vars, context: ctx) }
+
+      context 'to sign in user' do
+        let(:mutate_string) { %|mutation SignInUser($provider: String!) { signInUser(provider: $provider) { id uid name } }| }
+
+        it 'returns a user info' do
+          allow(social_api).to receive(:get_object).and_return(me)
+          result_user = result["data"]["signInUser"]
+          expect(result_user["id"]).not_to be_nil
+          expect(result_user["uid"]).to eq(me["id"])
+          expect(result_user["name"]).to eq(me["name"])
+        end
+      end
+    end
+  end
+end
