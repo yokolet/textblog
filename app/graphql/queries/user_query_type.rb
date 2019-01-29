a01001cc0779dc0495e6391ca21cfff913c913ca
Yoko Harada <yokolet@gmail.com>
2019-01-28 22:47:06 -0500

    chore: removes all user list
    This is a cleanup commit. Showing all users feature is just a test code
    to get started. This won't need once the app becomes realistic one.

--- app/graphql/queries/user_query_type.rb
+++ app/graphql/queries/user_query_type.rb
@@ -2,14 +2,6 @@ Queries::UserQueryType = GraphQL::ObjectType.define do
   name "UserQueryType"
   description "The User Query Type"
 
-  field :allUsers do
-    type types[Types::UserType]
-    description "returns a list of all users"
-    resolve -> (obj, args, ctx) {
-      User.all
-    }
-  end
-
   field :user do
     type Types::UserType
     description "returns a user"
--- spec/graphql/queries/user_query_type_spec.rb
+++ spec/graphql/queries/user_query_type_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 describe Queries::UserQueryType do
   types = GraphQL::Define::TypeDefiner.instance
 
-  it 'defines a field allUsers that returns an array of Types::UserType type' do
-    expect(subject).to have_a_field(:allUsers).that_returns(types[Types::UserType])
-  end
-
   it 'defines a field user that returns Types::UserType type' do
     expect(subject).to have_a_field(:user).that_returns(Types::UserType)
   end
@@ -14,12 +10,6 @@ describe Queries::UserQueryType do
   context 'with users' do
     let!(:users) { create_list(:user, 3) }
     let!(:a_user) { users.last }
-
-    it 'returns all users' do
-      result = subject.fields['allUsers'].resolve(nil, nil, nil)
-      expect(result.size).to eq(3)
-      expect(result.first.posts.size).to eq(2)
-    end
 
     it 'returns a user of id' do
       result = subject.fields['user'].resolve(nil, {id: a_user.id}, nil)
@@ -31,17 +21,6 @@ describe Queries::UserQueryType do
       let(:vars) { {} }
       let(:ctx) { {} }
       let(:result) { TextblogSchema.execute(query_string, variables: vars, context: ctx) }
-
-      context 'to request all user names' do
-        let(:query_string) { %|query { allUsers { name } }| }
-
-        it 'returns names' do
-          result_names = result["data"]["allUsers"].map { |x| x["name"] }
-          expect(result_names).to match_array(users.map(&:name))
-          result_ids = result["data"]["allUsers"].map { |x| x["id"] }
-          expect(result_ids).to match_array([nil]*3)
-        end
-      end
 
       context 'to request a single user' do
         let(:vars) { {"id": a_user.id} }
