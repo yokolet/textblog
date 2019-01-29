a59afcb1f6443a5c161442a68ff4bf714dba2bba
Yoko Harada <yokolet@gmail.com>
2019-01-29 17:01:34 -0500

    chore: adds length constrain to post's title and content
    The Post model got constrains on title and content lengths. Specs were
    added for the change.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   validates_presence_of :title, :content
+  validates_length_of :title, minimum: 1, maximum: 50
+  validates_length_of :content, minimum: 1, maximum: 5000
   belongs_to :user
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:content) }
+  it { should validate_length_of(:title).is_at_least(1).is_at_most(50) }
+  it { should validate_length_of(:content).is_at_least(1).is_at_most(5000) }
   it { should belong_to(:user) }
 end
