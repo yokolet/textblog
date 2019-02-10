2aaacfcae052e54015ad56d0bb9ca55fb81b8306
Yoko Harada <yokolet@gmail.com>
2019-02-09 22:01:00 -0500

    feat: adds footer
    To show a bit of site info, a footer was added.

--- app/javascript/packs/components/App.jsx
+++ app/javascript/packs/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './NavBar'
 import Main from './Main'
+import Footer from './Footer'
 
 const App = () => {
   return (
     <div>
       <NavBar/>
       <Main/>
+      <Footer />
     </div>
   )
 }
---
+++ app/javascript/packs/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <footer className="page-footer teal darken-1">
+      <div className="footer-copyright">
+        <div className="container">
+          © 2019 Yoko Harada
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://github.com/yokolet/textblog/blob/master/PRIVACY.md"
+          >
+            Privacy
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
