a1c30cf2594324880291fc07cb4c827724d9d2d6
Yoko Harada <yokolet@gmail.com>
2019-02-13 22:19:31 -0500

    chore: adjusts padding in SignIn component
    This page was left behind from previous style improvement. It is fixed
    now.

--- app/javascript/packs/components/SignIn.jsx
+++ app/javascript/packs/components/SignIn.jsx
@@ -7,12 +7,16 @@ class SignIn extends Component {
       marginTop: '20px'
     }
 
+    let panel_style = {
+      padding: '24px'
+    }
+
     let prev = this.props.location.state ? this.props.location.state.prevPath : "/"
 
     return (
       <div className="row" style={styles}>
         <div className="col s12 m12">
-          <div className="card-panel white">
+          <div className="card-panel white" style={panel_style}>
             <div  className="sign_in_container">
               <FacebookLoginButton prevPath={prev}/>
             </div>
