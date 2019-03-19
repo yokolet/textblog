8a4d769ca2c328cf1f58ffec89fb78839fd40f3c
Yoko Harada <yokolet@gmail.com>
2019-03-19 15:15:16 -0400

    chore: adds has_many spec to post
    A missing has_many comments spec for post model was added.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,4 +6,5 @@ RSpec.describe Post, type: :model do
   it { should validate_length_of(:title).is_at_least(1).is_at_most(50) }
   it { should validate_length_of(:content).is_at_least(1).is_at_most(5000) }
   it { should belong_to(:user) }
+  it { should have_many(:comments).dependent(:destroy) }
 end
